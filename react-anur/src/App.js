4cf739ceb670bb903605bf147c08626afd28dc23
anurag9034 <kamanurag5@gmail.com>
2022-01-17 09:27:12 -0800

    changed title from Anurag to APril

--- react-anur/src/App.js
+++ react-anur/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Anurag.
+          Edit <code>src/App.js</code> and save to reload April.
         </p>
         <a
           className="App-link"
