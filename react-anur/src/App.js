7c38ba8f3958bab4bb2a405a5ff63791815f32e4
anurag9034 <kamanurag5@gmail.com>
2022-01-17 09:09:20 -0800

    Changed title

--- react-anur/src/App.js
+++ react-anur/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React-Anurag
+          Learn React-Anurag Testing
         </a>
       </header>
     </div>
