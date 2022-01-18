42cdfc382eaf12709db8984a737ed213364df2f1
anurag9034 <kamanurag5@gmail.com>
2022-01-18 09:27:04 -0800

    changes to check conflicts

--- react-anur/src/App.js
+++ react-anur/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload April.
+          Edit1 <code>src/App.js</code> and save to reload April.
         </p>
         <a
           className="App-link"
